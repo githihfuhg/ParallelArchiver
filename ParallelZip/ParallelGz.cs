d7abd5ee3efeae36db932cd6ae955d6cada3649d
Ярослав <https://github.com/githihfuhg>
2020-01-24 12:50:49 +0300

    the first push

---
+++ ParallelZip/ParallelGz.cs
@@ -0,0 +1,120 @@
+﻿using ICSharpCode.SharpZipLib.Tar;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParallelArhive
+{
+
+    public class ParallelGz
+    {
+        private readonly int DegreeOfParallelism = Environment.ProcessorCount;
+        private List<byte> Date = new List<byte>();
+
+        public void CompressDirectory()
+        {
+
+        }
+        public void DecompessDirectory()
+        {
+
+        }
+
+        public void CompressFile(string Path)
+        {
+            //IEnumerable<string> paths = ...;
+            //var processTasks = paths.Select(p => Task.Run(() => ProcessFile(p));
+            //await Task.WhenAll(processTasks);
+            var date = FileReadByte(Path);
+
+
+
+
+
+
+
+
+
+        }
+
+        public void DecompressFile()
+        {
+
+        }
+
+        public byte[][] FileReadByte(string Path)
+        {
+           
+            var fileInfo = new FileInfo(Path);
+            long checkSize = fileInfo.Length / DegreeOfParallelism;
+            bool honesty = checkSize % 2 == 0;
+
+            var Bufer = Enumerable.Range(0, DegreeOfParallelism).Select(x => checkSize).ToList();
+
+            if (!honesty)
+            {
+                Bufer[0] += 3;
+            }
+
+            using var stream = fileInfo.OpenRead();
+            var parallelTask = Bufer.Select(x => Task.Factory.StartNew(() =>
+                {
+
+                    byte[] bytes = new byte[x];
+
+                    //Offset += (int)checkSize;
+                    stream.Read(bytes, 0, bytes.Length);
+
+                    return bytes;
+
+                }, TaskCreationOptions.LongRunning))
+                .ToArray();
+
+            Task.WaitAll(parallelTask);
+            return parallelTask.Select(x => x.Result).ToArray();
+
+        }
+
+        private void AddDirectoryFilesToTar(TarArchive tarArchive, string sourceDirectory, bool recurse)
+        {
+            TarEntry tarEntry = TarEntry.CreateEntryFromFile(sourceDirectory);
+            tarArchive.WriteEntry(tarEntry, false);
+
+
+            string[] filenames = Directory.GetFiles(sourceDirectory);
+            foreach (string filename in filenames)
+            {
+                tarEntry = TarEntry.CreateEntryFromFile(filename);
+                tarArchive.WriteEntry(tarEntry, true);
+            }
+
+            if (recurse)
+            {
+                string[] directories = Directory.GetDirectories(sourceDirectory);
+                foreach (string directory in directories)
+                    AddDirectoryFilesToTar(tarArchive, directory, recurse);
+            }
+        }
+
+        private void CreateTar(string tgzFilename, string sourceDirectory)
+        {
+            Stream outStream = File.Create(tgzFilename);
+            TarArchive tarArchive = TarArchive.CreateOutputTarArchive(outStream);
+            tarArchive.RootPath = sourceDirectory.Replace('\\', '/');
+            if (tarArchive.RootPath.EndsWith("/"))
+                tarArchive.RootPath = tarArchive.RootPath.Remove(tarArchive.RootPath.Length - 1);
+            AddDirectoryFilesToTar(tarArchive, sourceDirectory, true);
+            tarArchive.Close();
+        }
+
+        private void CreateGZip(List<byte> data)
+        {
+
+        }
+
+
+
+    }
+}
---
+++ test/Title.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace test
+{
+    internal class Title
+    {
+        public int FilePathLength { get; private set; } = 0;
+        public string FileName { get; private set; } = "";
+        public long FileLength { get; private set; } = 0;
+        public long PositionInTheStream { get; private set; } = 0;
+        public int BlockCount { get; private set; } = 0;
+
+
+        private FileStream Stream;
+        public Title(FileStream stream)
+        {
+            Stream = stream;
+        }
+        private Title() { }
+        private Title(Title title)
+        {
+            FilePathLength = title.FilePathLength;
+            FileName = title.FileName;
+            FileLength = title.FileLength;
+            PositionInTheStream = title.PositionInTheStream;
+            BlockCount = title.BlockCount;
+        }
+
+        public void AddTitleDirectories(DirectoryInfo mainDir)
+        {
+            var directories = mainDir.EnumerateDirectories("*", SearchOption.AllDirectories).ToList();
+            directories.Add(mainDir);
+            int DirCount = directories.Count();
+            Stream.Write(Encoding.UTF8.GetBytes("dir"));
+            Stream.Write(BitConverter.GetBytes(DirCount));
+            foreach (var dir in directories)
+            {
+                FileName = (dir == mainDir) ? 
+                    Path.Combine(mainDir.Name, dir.FullName.Replace($"{mainDir.FullName}", "")) + "/":
+                    Path.Combine(mainDir.Name, dir.FullName.Replace($"{mainDir.FullName}\\", "")) + "/";
+
+                var fileNameByte = Encoding.UTF8.GetBytes(FileName);
+                FilePathLength = fileNameByte.Length;
+                Stream.Write(BitConverter.GetBytes(FilePathLength));
+                Stream.Write(fileNameByte);
+            }
+        }
+        public void AddTitleFile(DirectoryInfo mainDir, string fullName, long fileLenght, int blockCount = 0)
+        {
+            FileName = Path.Combine(mainDir.Name, fullName.Replace($"{mainDir.FullName}\\", ""));
+            var fileNameByte = Encoding.UTF8.GetBytes(FileName);
+            FilePathLength = fileNameByte.Length;
+            FileLength = (fileLenght >= 52428800) ? 0 : fileLenght;
+            Stream.Write(BitConverter.GetBytes(FilePathLength));
+            Stream.Write(fileNameByte);
+            Stream.Write(BitConverter.GetBytes(FileLength));
+            if (FileLength == 0)
+            {
+                BlockCount = blockCount;
+                Stream.Write(BitConverter.GetBytes(BlockCount));
+            }
+            PositionInTheStream = Stream.Position;
+            Stream.Write(BitConverter.GetBytes(Stream.Position + 8));
+        }
+
+        public Title[] GetTitleDirectories()
+        {
+            Stream.Seek(3, SeekOrigin.Current);
+            var bufer = new byte[4];
+            Stream.Read(bufer);
+            var DirCount = BitConverter.ToInt32(bufer);
+            var titleDirectories = new Title[DirCount];
+            for (var i = 0; i < DirCount; i++)
+            {
+                try
+                {
+                    bufer = new byte[4];
+                    Stream.Read(bufer);
+                    FilePathLength = BitConverter.ToInt32(bufer);
+                    bufer = new byte[FilePathLength];
+                    Stream.Read(bufer,0,FilePathLength);
+                    FileName = Encoding.UTF8.GetString(bufer);
+                    titleDirectories[i] = new Title(this);
+                }
+                catch
+                {
+                    var a = 1;
+                }/* new Title(){FilePathLength = FilePathLength,FileName = FileName};*/
+            }
+            return titleDirectories;
+        }
+
+        public Title GetTitleFile(/*FileStream stream*/)
+        {
+            BlockCount = 0;
+            var buffer = new byte[4];
+            Stream.Read(buffer);
+            FilePathLength = BitConverter.ToInt32(buffer);
+            buffer = new byte[FilePathLength];
+            Stream.Read(buffer);
+            FileName = Encoding.UTF8.GetString(buffer);
+            buffer = new byte[8];
+            Stream.Read(buffer);
+            FileLength = BitConverter.ToInt64(buffer);
+            if (FileLength == 0)
+            {
+                Stream.Read(buffer, 0, 4);
+                BlockCount = BitConverter.ToInt32(buffer);
+            }
+            Stream.Read(buffer);
+            PositionInTheStream = BitConverter.ToInt64(buffer);
+
+            return this;
+        }
+
+        public Dictionary<string, Title> GetTitleFiles()
+        {
+            var titleFiles = new Dictionary<string, Title>();
+
+            //413867339
+            //830516399
+            while (Stream.Position < Stream.Length)
+            {
+                BlockCount = 0;
+                var buffer = new byte[4];
+                Stream.Read(buffer);
+                FilePathLength = BitConverter.ToInt32(buffer);
+                buffer = new byte[FilePathLength];
+                Stream.Read(buffer);
+                FileName = Encoding.UTF8.GetString(buffer);
+                buffer = new byte[8];
+                Stream.Read(buffer);
+                FileLength = BitConverter.ToInt64(buffer);
+                if (FileLength == 0)
+                {
+                    Stream.Read(buffer, 0, 4);
+                    BlockCount = BitConverter.ToInt32(buffer);
+                }
+
+                Stream.Read(buffer);
+                PositionInTheStream = BitConverter.ToInt64(buffer);
+
+                if (FileLength == 0)
+                {
+                    for (int i = 0; i < BlockCount; i++)
+                    {
+                        var Buffer = new byte[8];
+                        Stream.Read(Buffer);
+                        var blockLength = BitConverter.ToInt32(Buffer, 4);
+                        Stream.Seek(blockLength - 8, SeekOrigin.Current);
+                    }
+
+                }
+                else
+                {
+                    Stream.Seek(FileLength, SeekOrigin.Current);
+                }
+
+
+                titleFiles[FileName] = new Title(this);
+            }
+            //Stream.Seek(-Stream.Length, SeekOrigin.Current);
+            Stream.Position = 0;
+            return titleFiles;
+        }
+
+
+    }
+}
